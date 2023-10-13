a2568aad6ea19784c853e1daf4a77d2bfbe10e3c
mnpx01 <martinnietop01@gmail.com>
2023-10-13 20:07:37 +0200

    Algo regular
    20:07

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,11 @@
 <!-- Bootstrap first template for Internet y Negocio Electrónico, University of Cadiz,
      since 2019, based on https://www.w3schools.com/bootstrap4/bootstrap_templates.asp -->
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
+
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">      
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
     <!-- 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -13,111 +14,176 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script> 
     -->
     <style>
-      .fakeimg { height: 100px; background: #aaa; }
+        header {
+            margin-bottom: 0;
+            padding: 20px;
+            background-color: lightgreen;
+        }
+
+        footer {
+            margin-top: 0;
+            padding: 20px;
+            background-color: lightgreen;
+        }
+
+        a.icon-link {
+            color: black;
+            font-size: 24px;
+            text-decoration: none;
+        }
+
+        .cardclass {
+            width: 80px;
+            height: 80px;
+        }
     </style>
 
-    <title>My INE project</title>
-  </head>
+    <title>Arbi Inc.</title>
+</head>
 
-  <body>
+<body>
 
     <!-- LAYOUT: HEADER -->
 
-      <!-- SECTION: Title -->
-      <div class="jumbotron text-center" style="margin-bottom:0;padding:20px;">
-        <h1>My INE project in Laravel/Bootstrap</h1>
-        <p>Resize this responsive page to see the effect!</p>
-      </div>
+    <!-- SECTION: Title -->
+    <header class="jumbotron">
+        <nav class="navbar navbar-expand-sm">
+            <div class="container-fluid">
+                <h1>Arbi Inc. Shop</h1>
+            </div>
+            <div class="container-fluid">
+                <form class="d-flex form-inline" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+            <div class="container-fluid">
+                <a class="icon-link" href="#"> Autenticación </a>
+            </div>
+            <div class="container-fluid">
+                <i class="bi bi-cart"></i>
+            </div>
+        </nav>
+    </header>
 
-      <!-- SECTION: Main menu -->
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Link</a></li>    
-          </ul>
-        </div>  
-      </nav>
-
-    <!-- LAYOUT: CENTER -->
-    <div class="container-fluid" style="margin-top:30px">
-      <div class="row">
-
-        <!-- BLOCK: LEFT -->
-        <div class="col-sm-3"> <!-- col-sm-3 means three out of twelve columns -->
-
-          <!-- SECTION: About me -->
-          <h2>About Me</h2>
-          <h5>Photo of me:</h5>
-          <div class="fakeimg">Fake Picture</div>
-          <p>Some text about me in culpa qui officia deserunt mollit anim.</p>
-          
-          <!-- SECTION: Some links -->
-          <h3>Some Links</h3>
-          <p>Lorem ipsum dolor sit ame.</p>
-          <ul class="nav nav-pills flex-column">
-            <li class="nav-item"><a class="nav-link active" href="#">Active</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
-            <li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a></li>
-          </ul>
-          <hr class="d-sm-none"/> <!-- d-sm-none means that the element will show only when the screen is small -->
+    <div class="container-fluid">
+        <h3> Ofertas del día </h3>
+        <div class="row">
+            <div class="col-sm-2 card card-body" id="cardclass">
+                <img src="img/Brazalete.jpg">
+                <div class="card-body">
+                    <h5 class="card-title">Brazalete</h5>
+                    <p class="card-text">Brazalete oficial de capitan.</p>
+                </div>
+            </div>
+            <div class="col-sm-2 card card-body" id="cardclass">
+                <img src="img/Spray.jpg">
+                <div class="card-body">
+                    <h5 class="card-title">Spray arbitro</h5>
+                    <p class="card-text">Spray que respeta el crecimientmo del cesped natural.</p>
+                </div>
+            </div>
+            <div class="col-sm-2 card card-body" id="cardclass">
+                <img src="img/Toalla.jpg">
+                <div class="card-body">
+                    <h5 class="card-title">Toalla</h5>
+                    <p class="card-text">Toalla de viaje, para cada fin de semana.</p>
+                </div>
+            </div>
+            <div class="col-sm-2 card card-body" id="cardclass">
+                <img src="img/Tarjetas.jpg">
+                <div class="card-body">
+                    <h5 class="card-title">Pack Tarjetas</h5>
+                    <p class="card-text">Maravillosas tarjetas para expulsa a gente.</p>
+                </div>
+            </div>
         </div>
-
-        <!-- BLOCK: CENTER -->
-        <div class="col-sm-7"> <!-- col-sm-7 means seven out of twelve columns -->
-          <!-- SECTION: Entries -->          
-          <h2>TITLE HEADING 1</h2>
-          <h5>Title description 1, Dec 7, 2018</h5>
-          <div class="fakeimg">Fake Image 1</div> <!-- fakeimg is defined in the <style> tag, at the beginning -->
-          <p>Some text...</p>
-          <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-          <div class="alert alert-warning" role="alert">
-            <strong>Warning!</strong> Example warning for entry 1.
-          </div>
-          <br/>
-          <h2>TITLE HEADING 2</h2>
-          <h5>Title description 2, Sep 2, 2019</h5>
-          <div class="fakeimg">Fake Image 2</div>
-          <p>Some text...</p>
-          <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-          <br/>
-        </div>
-
-        <!-- BLOCK: RIGHT -->
-        <div class="col-sm-2 sidenav"> <!-- col-sm-2 means two out of twelve columns -->
-          <!-- SECTION: Cards -->
-          <h2>Cards</h2>
-          <!-- Style are directly included in the style attribute just for illustrative reasons. -->
-          <!-- However, including them in <style> or in a CSS file is a better practice. -->
-          <div class="card card-body bg-faded" style="background-color: #0ff; margin-bottom:15px;">
-            <h4 class="card-title">Card 1</h4>
-            <p class="card-text">Text of card 1.</p>
-            <a href="#" class="btn btn-primary">This is a Button</a>
-          </div>           
-          <div class="card card-body bg-light" style="margin-bottom:15px;">
-            <h4 class="card-title">Card 2</h4>
-            <div class="fakeimg">Fake card 2 image</div>
-            <p class="card-text">Text of card 2.</p>
-          </div>           
-        </div>
-      </div>
     </div>
+
+    <div class="container-fluid">
+        <h3> Nuevos productos </h3>
+        <div class="row">
+            <div class="col-sm-2 card card-body" id="cardclass">
+                <img src="img/Brazalete.jpg">
+                <div class="card-body">
+                    <h5 class="card-title">Brazalete</h5>
+                    <p class="card-text">Brazalete oficial de capitan.</p>
+                </div>
+            </div>
+            <div class="col-sm-2 card card-body" id="cardclass">
+                <img src="img/Spray.jpg">
+                <div class="card-body">
+                    <h5 class="card-title">Spray arbitro</h5>
+                    <p class="card-text">Spray que respeta el crecimientmo del cesped natural.</p>
+                </div>
+            </div>
+            <div class="col-sm-2 card card-body" id="cardclass">
+                <img src="img/Toalla.jpg">
+                <div class="card-body">
+                    <h5 class="card-title">Toalla</h5>
+                    <p class="card-text">Toalla de viaje, para cada fin de semana.</p>
+                </div>
+            </div>
+            <div class="col-sm-2 card card-body" id="cardclass">
+                <img src="img/Tarjetas.jpg">
+                <div class="card-body">
+                    <h5 class="card-title">Pack Tarjetas</h5>
+                    <p class="card-text">Maravillosas tarjetas para expulsa a gente.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <!-- LAYOUT: FOOTER -->
-    <div class="jumbotron text-center" style="margin-bottom:0;padding:20px">
-      <p>Footer</p>
-    </div>
+    <footer class="row border-top">
+        <div class="col">
+            <h5>Acerca de</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Quiénes somos</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Trabaja con nosotros</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Aviso legal</a></li>
+            </ul>
+        </div>
 
-    <!-- Loading Javascripts -->   
+        <div class="col">
+            <h5>Navegación</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Idioma/ languajes</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Mapa del sitio</a></li>
+        </div>
+
+        <div class="col">
+            <h5>Ayuda</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Gestionar compras</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Tarifas y políticas de envío</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Devolver o reemplazar productos</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Contacto para ayuda</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Iva sobre los bienes</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Método de pago</a></li>
+            </ul>
+        </div>
+
+        <div class="col">
+            <h5>Contacto</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Atención al cliente</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Twitter
+                        <svg class="bi me-2" width="30" height="25">
+                            <use xlink:href="#twitter" />
+                        </svg>
+                    </a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Facebook</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Youtube</a></li>
+                <li class="nav-item"><a href="#" class="nav-link p-0 text-body-secondary">Instagram</a></li>
+            </ul>
+        </div>
+    </footer>
+
+
+    <!-- Loading Javascripts -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>')</script>
-    <!-- <script src="../../assets/js/vendor/popper.min.js"></script> -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+</body>
+
 </html>
